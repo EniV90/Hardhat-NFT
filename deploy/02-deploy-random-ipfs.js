6c88b04d5ba67506e24991cb3df4dc576d1b66d7
EniV90 <eni_v90@yahoo.com>
2023-01-05 20:53:37 +0100

    feat: update

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -16,6 +16,8 @@ const metadataTemplate = {
     ],
 }
 
+const FUND_AMOUNT = "100000000"
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -26,7 +28,11 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     // 1. my own IPFS NODE. https://docs.ipfs.io/
     // 2. Pinata
     // 3. NFT.storage
-    let tokenUris
+    let tokenUris = [
+        "ipfs://QmQs4yASJakykKzcUYiJoQEFptCuufghNA3S5J2CkD47tp",
+        "ipfs://QmXry9jwWVKfbt6V87Gzd97WJ5LGAmtyWY7znSQXCRysv9",
+        "ipfs://QmX5V7Xc31vMfM8tYgrNefix1WCFmiMqpLzjDtk6PgTQd2",
+    ]
     if (process.env.UPLOAD_TO_PINATA == "true") {
         tokenUris = await handleTokenUris()
     }
@@ -39,6 +45,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         const tx = await vrfcoordinatorV2Mock.createSubscription()
         const txReceipt = await tx.wait(1)
         subscriptionId = txReceipt.events[0].args.subId
+        await vrfcoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfcoordinatorV2Address = networkConfig[chainId].vrfcoordinatorV2
         subscriptionId = networkConfig[chainId].subscriptionId
@@ -54,6 +61,18 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         tokenUris,
         networkConfig[chainId].callbackGasLimit,
     ]
+
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log("----------------------------------------")
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(randomIpfsNft.address, args)
+    }
 }
 
 async function handleTokenUris() {
@@ -70,16 +89,14 @@ async function handleTokenUris() {
 
         tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
         tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
-        tokenUriMetadata.image =`ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
         console.log(`Uploading${tokenUriMetadata.name}...`)
-
 
         // store the json to pinata IPFS
         const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
         tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
         console.log("Token URIs Uploaded! They are:")
         console.log(tokenUris)
-
     }
 
     return tokenUris
